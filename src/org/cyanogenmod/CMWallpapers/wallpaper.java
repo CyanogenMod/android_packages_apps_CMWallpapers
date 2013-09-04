adab18dc90ac981e2a96e793d8e0c7db0e872cbe
something15525 <something15525@gmail.com>
2013-09-04 15:29:49 -0700

    CMWallpapers: rename from com to org
    CM is an organization, therefore we should be org.cyanogenmod,
    not com.cyanogenmod anymore :)
    
    Change-Id: I5dfa521894138072d14fb2984622d14a5f7ab09f

--- src/org/cyanogenmod/CMWallpapers/wallpaper.java
+++ src/org/cyanogenmod/CMWallpapers/wallpaper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.CMWallpapers;
+package org.cyanogenmod.CMWallpapers;
 
 import android.app.Activity;
 import android.content.res.Resources;
